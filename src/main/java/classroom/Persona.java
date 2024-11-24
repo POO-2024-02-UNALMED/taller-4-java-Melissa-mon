e0139672a4d06e06d54a8b1e8b265391817ff7ae
Melissa-mon <71413547+Melissa-mon@users.noreply.github.com>
2024-11-24 22:04:40 +0000

    jesuuuuuuuuuuuuuuuuuuu

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,10 +2,11 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+
+    long cedula = 1;
     String nombre;
     static int totalPersonas;
-
+    
     static {
         totalPersonas = 0;
     }
@@ -16,26 +17,45 @@ public class Persona {
         totalPersonas++;
     }
     
+
     public Persona(String nombre, long cedula) {
-        this(cedula, nombre);
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
     }
+
 
     public Persona(long cedula) {
-        this(cedula, "");
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
     }
 
+
     public Persona(String nombre) {
-        this(0, nombre);
+        this.nombre = nombre;
+        this.cedula = 1;
+        totalPersonas++;
     }
     
+
+    public Persona(){
+        this.cedula = 0;
+        this.nombre = "";
+        totalPersonas++;
+    }
+
+
     public long getCedula() {
         return cedula;
     }
+
 
     public String getNombre() {
         return nombre;
     }
 
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
