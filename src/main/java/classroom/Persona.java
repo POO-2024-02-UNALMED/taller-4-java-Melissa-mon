650c2340b6e29b1308d9d4c40edb6883ce16831f
Melissa-mon <anmontoyau@unal.edu.co>
2024-11-24 16:54:54 -0500

    AYAYYYAA

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -5,10 +5,9 @@ public class Persona {
     final long cedula;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -18,20 +17,15 @@ public class Persona {
     }
     
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
+        this(cedula, nombre);
     }
 
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
+        this(cedula, "");
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
+        this(0, nombre);
     }
     
     public long getCedula() {
@@ -46,3 +40,4 @@ public class Persona {
         this.nombre = nombre;
     }
 }
+
--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,15 +1,16 @@
 package classroom;
 
 public enum Tipo {
-    DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
+	DISCIPLINAR(10, "Discicplinar"),FUNDAMENTACION(20, "Fundamentacion"),ELECTIVA(30, "Electiva");
+	
 
-    private int codigo;
+	private int codigo;
+	private String nombre;
 
-    Tipo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
+    
+	 Tipo(int codigo,String nombre) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+	
 }
